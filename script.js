c78893d4f2f597a2022b671463e729104e946545
rahmafallous2 <rahmifallous@gmail.com>
2025-08-14 14:27:55 +0300

    Define all the needed variables

---
+++ script.js
@@ -0,0 +1,10 @@
+const taskForm = document.getElementById('taskform');
+const taskInput = document.getElementById('taskinput');
+const taskList = document.getElementById('tasklist');
+const errorMsg = document.getElementById('error');
+const filterBtns = document.querySelectorAll('.filters button');
+
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+let currentFilter = 'all';
+
+
