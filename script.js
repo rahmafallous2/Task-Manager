f6246da464876f2dcfda01e5fee0e2512cba5e94
rahmafallous2 <rahmifallous@gmail.com>
2025-08-15 14:57:00 +0300

    Add Task Function

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
-const taskForm = document.getElementById('taskform');
-const taskInput = document.getElementById('taskinput');
-const taskList = document.getElementById('tasklist');
-const errorMsg = document.getElementById('error');
-const filterBtns = document.querySelectorAll('.filters button');
+let taskForm = document.getElementById('taskform');
+let taskInput = document.getElementById('taskinput');
+let taskList = document.getElementById('tasklist');
+let errorMsg = document.getElementById('error');
+let filterBtns = document.querySelectorAll('.filters button');
 
 let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 let currentFilter = 'all';
 
-
 function saveTasks(){
-    localStorage.setItem('tasks',JSON.stringify(tasks));
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 }
+
+taskForm.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    let text = taskInput.value.trim();
+
+    if(!text){
+        errorMsg.textContent = "Task can't be empty!";
+        return;
+    }
+    errorMsg.textContent = "";
+
+    let newTask = {
+        id: Date.now(),
+        text: text,
+        done: false
+    };
+
+    tasks.push(newTask);
+    taskInput.value = "";
+    saveTasks();
+   
+});
+
